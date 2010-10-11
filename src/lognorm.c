2bc7ba1e26fa7c9769adcd948a7d8b24f1375865
Rainer Gerhards <rgerhards@adiscon.com>
2010-10-11 18:05:46 +0200

    generated initial skeleton project

---
+++ src/lognorm.c
@@ -0,0 +1,25 @@
+/* liblognorm - a fast samples-based log normalization library
+ * Copyright 2010 by Rainer Gerhards and Adiscon GmbH.
+ *
+ * This file is part of liblognorm.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * A copy of the LGPL v2.1 can be found in the file "COPYING" in this distribution.
+ *
+ * If the terms of the GPL are unsuitable for your needs, you may obtain
+ * a commercial license from Adiscon. Contact sales@adiscon.com for further
+ * details.
+ */
