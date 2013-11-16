rsyslog/liblognorm

commit 3a94f64652d77a4d3f0095285c192e8c56304647

Fix incorrect parsing of RFC5424 at end of string

src/parser.c
@@ -37,7 +37,7 @@ hParseInt(unsigned char **buf, es_size_t *lenBuf)
 	unsigned char *p = *buf;
 	es_size_t len = *lenBuf;
 	int i = 0;
-	
+
 	while(len > 0 && isdigit(*p)) {
 		i = i * 10 + *p - '0';
 		++p;
@@ -175,6 +175,7 @@ BEGINParser(RFC5424Date)
 
 		if(len == 0 || *pszTS++ != ':')
 			goto fail;
+		--len;
 		OffsetMinute = hParseInt(&pszTS, &len);
 		if(OffsetMinute < 0 || OffsetMinute > 59)
 			goto fail;

commit b9d009c5913da8d3257771ef7ca6c1afb1a62c5f

Fix a regression caused by 3a94f64652d77a4d3f0095285c192e8c56304647

src/parser.c
@@ -185,8 +185,6 @@ BEGINParser(RFC5424Date)
 	if(len > 0) {
 		if(*pszTS != ' ') /* if it is not a space, it can not be a "good" time */
 			goto fail;
-		++pszTS; /* just skip past it */
-		--len;
 	}
 
 	/* we had success, so update parse pointer */