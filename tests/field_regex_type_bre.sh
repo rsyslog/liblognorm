1797bb460ab6f36ae869551042d4f8c99a9a2e9a
Janmejay Singh <singh.janmejay@gmail.com>
2014-11-17 11:20:42 +0530

    moved from regex to pcre because of confusion/wierdness in handling of sub-matches

--- tests/field_regex_type_bre.sh
+++
@@ -1,7 +0,0 @@
-# added 2014-11-14 by singh.janmejay
-# This file is part of the liblognorm project, released under ASL 2.0
-desc="type BRE for regex field"
-source exec.sh
-echo 'foo bar' | execute
-assert_output_is '{ "first" : "foo", "second" : "bar" }'
-
--- tests/field_regex_type_defaulting.sh
+++
@@ -1,7 +0,0 @@
-# added 2014-11-14 by singh.janmejay
-# This file is part of the rsyslog project, released under ASL 2.0
-desc="type default for regex field"
-source exec.sh
-echo 'foo bar' | execute
-assert_output_is '{ "first" : "foo", "second" : "bar" }'
-
