018c1c4568aadd8a02f4656c38635354d0703f30
Janmejay Singh <singh.janmejay@gmail.com>
2014-11-17 12:51:24 +0530

    added some tests for regex with negation

---
+++ tests/field_regex_with_negation.sh
@@ -0,0 +1,12 @@
+# added 2014-11-17 by singh.janmejay
+# This file is part of the liblognorm project, released under ASL 2.0
+source ./exec.sh $0 "regex field with negation"
+add_rule 'rule=:%text:regex:[^,]+%,%more:rest%'
+execute '123,abc'
+assert_output_contains '"text": "123"'
+assert_output_contains '"more": "abc"'
+reset_rules
+add_rule 'rule=:%text:regex:([^ ,|]+( |\||,)?)+%%more:rest%'
+execute '123 abc|456 789,def|ghi,jkl| and some more text'
+assert_output_contains '"text": "123 abc|456 789,def|ghi,jkl|"'
+assert_output_contains '"more": " and some more text"'
