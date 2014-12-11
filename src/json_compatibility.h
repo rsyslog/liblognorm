ed573c8ab17ff1f66ca28804d146a2c877140337
Janmejay Singh <singh.janmejay@gmail.com>
2014-12-11 14:28:03 +0530

    interpret field support for older versions of json-c

--- src/json_compatibility.h
+++ src/json_compatibility.h
@@ -1,4 +1,5 @@
 #include <json.h>
+#include <inttypes.h>
 
 #ifndef HAVE_JSON_OBJECT_OBJECT_GET_EX
 int json_object_object_get_ex(struct json_object* obj,
@@ -8,4 +9,16 @@ int json_object_object_get_ex(struct json_object* obj,
 
 #ifndef HAVE_JSON_OBJECT_GET_STRING_LEN
 int json_object_get_string_len(struct json_object *obj);
+#endif
+
+#ifndef HAVE_JSON_BOOL
+typedef int json_bool;
+#endif
+
+#ifndef HAVE_JSON_OBJECT_GET_INT64
+int64_t json_object_get_int64(struct json_object *jso);
+#endif
+
+#ifndef HAVE_JSON_OBJECT_NEW_INT64
+struct json_object* json_object_new_int64(int64_t i);
 #endif
