b06fc1accd5249ea4b677d8b4d4719028558e0cb
Rainer Gerhards <rgerhards@adiscon.com>
2015-04-14 12:28:07 +0200

    cisco-interface-spec parser: add optional user part

--- tests/field_cisco-interface-spec.sh
+++ tests/field_cisco-interface-spec.sh
@@ -5,3 +5,15 @@ add_rule 'rule=:begin %field:cisco-interface-spec% end'
 
 execute 'begin outside:176.97.252.102/50349 end'
 assert_output_json_eq '{"field": "outside:176.97.252.102/50349"}'
+
+execute 'begin outside:176.97.252.102/50349(DOMAIN\rainer) end'
+# we need to add the backslash escape for the testbench plumbing
+assert_output_json_eq '{"field": "outside:176.97.252.102/50349(DOMAIN\\rainer)"}'
+
+execute 'begin outside:176.97.252.102/50349(test/rainer) end'
+# we need to add the backslash escape for the testbench plumbing
+assert_output_json_eq '{"field": "outside:176.97.252.102/50349(test/rainer)"}'
+
+execute 'begin outside:176.97.252.102/50349(rainer) end'
+# we need to add the backslash escape for the testbench plumbing
+assert_output_json_eq '{"field": "outside:176.97.252.102/50349(rainer)"}'
