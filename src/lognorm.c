0a1e477608452f514054060114db7cef501c59bc
Rainer Gerhards <rgerhards@adiscon.com>
2015-07-15 17:20:44 +0200

    add capability to interpret errno to error reporting callback
    using some code taken from ASL 2.0 parts of rsyslog

--- src/lognorm.c
+++ src/lognorm.c
@@ -23,12 +23,46 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include "json_compatibility.h"
 #include "liblognorm.h"
 #include "lognorm.h"
 #include "ptree.h"
 
+/* Code taken from rsyslog ASL 2.0 code.
+ * From varmojfekoj's mail on why he provided rs_strerror_r():
+ * There are two problems with strerror_r():
+ * I see you've rewritten some of the code which calls it to use only
+ * the supplied buffer; unfortunately the GNU implementation sometimes
+ * doesn't use the buffer at all and returns a pointer to some
+ * immutable string instead, as noted in the man page.
+ *
+ * The other problem is that on some systems strerror_r() has a return
+ * type of int.
+ *
+ * So I've written a wrapper function rs_strerror_r(), which should
+ * take care of all this and be used instead.
+ */
+static char *
+rs_strerror_r(const int errnum, char *const buf, const size_t buflen) {
+#ifndef HAVE_STRERROR_R
+	char *pszErr;
+	pszErr = strerror(errnum);
+	snprintf(buf, buflen, "%s", pszErr);
+#else
+#	ifdef STRERROR_R_CHAR_P
+		char *p = strerror_r(errnum, buf, buflen);
+		if (p != buf) {
+			strncpy(buf, p, buflen);
+			buf[buflen - 1] = '\0';
+		}
+#	else
+		strerror_r(errnum, buf, buflen);
+#	endif
+#endif
+	return buf;
+}
 /**
  * Generate some debug message and call the caller provided callback.
  *
@@ -64,9 +98,8 @@ done:	return;
 
 /**
  * Generate error message and call the caller provided callback.
- * eno is the OS errno. If 0 (OK), it is not output. If non-zero
- * it will be output (and in later versions also with descriptive
- * text).
+ * eno is the OS errno. If non-zero, the OS error description
+ * will be added after the user-provided string.
  *
  * Will first check if a user callback is registered. If not, returns
  * immediately.
@@ -76,7 +109,10 @@ ln_errprintf(const ln_ctx ctx, const int eno, const char *fmt, ...)
 {
 	va_list ap;
 	char buf[8*1024];
+	char errbuf[1024];
+	char finalbuf[9*1024];
 	size_t lenBuf;
+	char *msg;
 
 	if(ctx->errmsgCB == NULL)
 		goto done;
@@ -94,7 +130,15 @@ ln_errprintf(const ln_ctx ctx, const int eno, const char *fmt, ...)
 		lenBuf = sizeof(buf) - 1;
 	}
 
-	ctx->errmsgCB(ctx->dbgCookie, buf, lenBuf);
+	if(eno != 0) {
+		rs_strerror_r(eno, errbuf, sizeof(errbuf));
+		lenBuf = snprintf(finalbuf, sizeof(finalbuf), "%s: %s", buf, errbuf);
+		msg = finalbuf;
+	} else {
+		msg = buf;
+	}
+
+	ctx->errmsgCB(ctx->dbgCookie, finalbuf, lenBuf);
 done:	return;
 }
 
