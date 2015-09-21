fb00f73ce6680089829a51ad5460357c921a52da
Janmejay Singh <singh.janmejay@gmail.com>
2015-09-21 18:46:13 +0530

    lognormalizer now works with log-messages longer than 10k (older hardcoded buffer sz)

--- src/lognormalizer.c
+++ src/lognormalizer.c
@@ -149,13 +149,40 @@ amendLineNbr(json_object *const json, const int line_nbr)
 	}
 }
 
+#define DEFAULT_LINE_SIZE (10 * 1024)
+
+char* read_line(FILE *fp) {
+	size_t line_capacity = DEFAULT_LINE_SIZE;
+	char *line = NULL;
+	size_t line_len = 0;
+	char ch = 0;
+	do {
+		ch = fgetc(fp);
+		if (ch == EOF) break;
+		if (line == NULL) {
+			line = malloc(line_capacity);
+		} else if (line_len == line_capacity) {
+			line_capacity *= 2;
+			line = realloc(line, line_capacity);
+		}
+		line[line_len++] = ch;
+	} while(ch != '\n');
+
+	if (line != NULL) {
+		line[--line_len] = '\0';
+		if(line_len > 0 && line[line_len - 1] == '\r')
+			line[--line_len] = '\0';
+	}
+	return line;
+}
+
 /* normalize input data
  */
 void
 normalize(void)
 {
 	FILE *fp = stdin;
-	char buf[10*1024];
+	char *line = NULL;
 	struct json_object *json = NULL;
 	long long unsigned numParsed = 0;
 	long long unsigned numUnparsed = 0;
@@ -167,13 +194,10 @@ normalize(void)
 		mandatoryTagCstr = es_str2cstr(mandatoryTag, NULL);
 	}
 
-	while((fgets(buf, sizeof(buf), fp)) != NULL) {
+	while((line = read_line(fp)) != NULL) {
 		++line_nbr;
-		buf[strlen(buf)-1] = '\0';
-		if(strlen(buf) > 0 && buf[strlen(buf)-1] == '\r')
-			buf[strlen(buf)-1] = '\0';
-		if(verbose > 0) fprintf(stderr, "To normalize: '%s'\n", buf);
-		ln_normalize(ctx, buf, strlen(buf), &json);
+		if(verbose > 0) fprintf(stderr, "To normalize: '%s'\n", line);
+		ln_normalize(ctx, line, strlen(line), &json);
 		if(json != NULL) {
 			if(eventHasTag(json, mandatoryTagCstr)) {
 				struct json_object *dummy;
@@ -197,6 +221,7 @@ normalize(void)
 			json_object_put(json);
 			json = NULL;
 		}
+        free(line);
 	}
 	if((recOutput & OUTPUT_PARSED_RECS) && numUnparsed > 0)
 		fprintf(stderr, "%llu unparsable entries\n", numUnparsed);
