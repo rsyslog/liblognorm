296c0ba1332174e2a80dbfe0d92cd95e01183427
Rainer Gerhards <rgerhards@adiscon.com>
2016-10-17 13:02:58 +0200

    testbench: add test for "alternative" parser literal path compaction
    This tests for the bug that was fixed with the last commit.

--- tests/alternative_segfault.sh
+++ tests/alternative_segfault.sh
@@ -5,8 +5,12 @@
 
 test_def $0 "a case that caused a segfault in practice"
 add_rule 'version=2'
-add_rule 'rule=:%host:ipv4% %{"type":"alternative","parser":[{"type":"literal","text":"-"},{"type":"word","name":"identd"}]}% %r:rest%'
-execute '1.2.3.4 - - [23/Sep/2016:11:12:50 +0200] \"GET /app/img/oldlogo.png HTTP/1.1\" 304 -'
-assert_output_json_eq '{ "hex": "0x4711" }'
+add_rule 'rule=:%host:ipv4% %{"type":"alternative","parser":[{"type":"literal","text":"-"},{"type":"number","name":"identd"}]}% %OK:word%'
+execute '1.2.3.4 - TEST_OK'
+assert_output_json_eq '{ "OK": "TEST_OK", "host": "1.2.3.4" }'
+execute '1.2.3.4 100 TEST_OK'
+assert_output_json_eq '{ "OK": "TEST_OK", "identd": "100", "host": "1.2.3.4" }'
+execute '1.2.3.4 ERR TEST_OK'
+assert_output_json_eq '{ "originalmsg": "1.2.3.4 ERR TEST_OK", "unparsed-data": "ERR TEST_OK" }'
 
 cleanup_tmp_files
