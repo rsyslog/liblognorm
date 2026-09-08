2a4812be2e27194530451b0b3a7f0af0c16da396
Rainer Gerhards <rgerhards@adiscon.com>
2026-09-08 12:55:28 +0200

    Fix shellcheck failure in RFC5424 regression test
    Quote harness-provided paths and test names, and document the intentional shellcheck exemptions for the test harness variables. This keeps PR 421 CI green without changing parser behavior.
    
    With the help of AI Agent: GPT-5

--- tests/field_rfc5424timestamp-fmt_timestamp-unix-ms.sh
+++ tests/field_rfc5424timestamp-fmt_timestamp-unix-ms.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # added 2017-10-02 by Rainer Gerhards
 # This file is part of the liblognorm project, released under ASL 2.0
+# shellcheck disable=SC1091,SC2154
 
-. $srcdir/exec.sh
+. "$srcdir/exec.sh"
 
-test_def $0 "RFC5424 timestamp in timestamp-unix format"
+test_def "$0" "RFC5424 timestamp in timestamp-unix format"
 add_rule 'version=2'
 add_rule 'rule=:here is a timestamp %{ "type":"date-rfc5424", "name":"num", "format":"timestamp-unix-ms"}% in RFC5424 format'
 execute 'here is a timestamp 2000-03-11T14:15:16+01:00 in RFC5424 format'
